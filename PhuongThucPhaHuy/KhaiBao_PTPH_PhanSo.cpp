226336eebda9fedf6a062af79e514f55e8ed4bc8
hhMy <hhmy1995@gmail.com>
2024-01-18 22:32:30 +0700

    hiding commit

--- PhuongThucPhaHuy/KhaiBao_PTPH_PhanSo.cpp
+++ PhuongThucPhaHuy/KhaiBao_PTPH_PhanSo.cpp
@@ -1,19 +1,23 @@
-//Hay khai bao va dinh nghia phuong thuc pha huy cho lop doi tuong CPhanSo
+// Hay khai bao va dinh nghia phuong thuc pha huy cho lop doi tuong CPhanSo
 
 #include <iostream>
 using namespace std;
-class CPhanSo {
+class CPhanSo
+{
 private:
 	int tu;
 	int mau;
+
 public:
-	~CPhanSo(); //Nhom phuong thuc xu ly
+	~CPhanSo(); // Nhom phuong thuc xu ly
 };
-CPhanSo::~CPhanSo() {
+CPhanSo::~CPhanSo()
+{
 	return;
 }
 
-int main() {
-
+int main()
+{
+	/// hiding comment from My's Branch kkk
 	return 1;
 }
